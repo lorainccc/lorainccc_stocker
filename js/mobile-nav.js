f1888cae69c43e75cd520916ae0e41ad8ffa79f1
Joe <joecue@gmail.com>
2019-03-28 16:08:54 -0400

    Consolidating Menus and Search Widgets
    Removed instances of mobile top header nav menu and mobile search widget.

---
+++ js/mobile-nav.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ *	Checking for mobile screen size, alter top menu to function for mobile.
+ * 
+ * 
+ */
+
+jQuery(document).ready(function(){
+	if (jQuery(window).width() < 720 ){
+		jQuery('#menu-primary-menu').removeClass('dropdown menu');
+		jQuery('#menu-primary-menu').wrap("<div class='is-drilldown' style='min-height: 175px; max-width: 400;px;'></div>");
+		jQuery('#menu-primary-menu').wrap("<ul id='mobile-menu' class='vertical menu' data-drilldown data-parent-link='true' role='menubar'></ul>");
+		jQuery('#menu-primary-menu').addClass('vertical menu');
+		jQuery('#menu-primary-menu').removeAttr('data-dropdown-menu');
+		jQuery('#menu-primary-menu').removeAttr('role');
+		jQuery('ul#menu-primary-menu').removeClass('opens-right');
+		jQuery('.first-sub').removeClass('first-sub');
+		jQuery('ul#menu-primary-menu li').removeAttr('id');
+		jQuery('.is-dropdown-submenu-parent').addClass('is-drilldown-submenu-parent').removeClass('is-dropdown-submenu-parent opens-right');
+		jQuery('.is-dropdown-submenu-item').addClass('is-submenu-parent-item is-drilldown-submenu-item').removeClass('is-dropdown-submenu-item');
+		jQuery('.is-dropdown-submenu').addClass('is-drilldown-submenu').removeClass('is-dropdown-submenu');
+		jQuery('#responsive-menu').append("<ul id='mobile-menu' class='vertical menu'></ul>")
+		jQuery('#header-menu').children().clone().appendTo('#mobile-menu');
+	}
+
+ });
