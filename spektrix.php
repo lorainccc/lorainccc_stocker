9db196333b78279ed8f3be4333961d511dcc42f2
Joe Querin <joecue@gmail.com>
2021-03-17 10:22:25 -0400

    :wrench: Removing Temp iFrame Changes

--- spektrix.php
+++ spektrix.php
@@ -69,7 +69,7 @@ get_header(); ?>
 
 				switch($iframeurl){
 					case "ChooseSeats":
-						echo '    <iframe style="width:600px; min-height: 600px; border:0;" src="' . $url . 'ChooseSeats.aspx?' . $style . '&resize=true&EventInstanceId=' . $lc_event_id .'" onload="setTimeout(function(){ window.scrollTo(0,0);}, 100)" title="LCCC Stocker Art Center\'s Choose Seats page in Spektrix"></iframe>';
+						echo '    <iframe src="' . $url . 'ChooseSeats.aspx?' . $style . '&resize=true&EventInstanceId=' . $lc_event_id .'" onload="setTimeout(function(){ window.scrollTo(0,0);}, 100)" title="LCCC Stocker Art Center\'s Choose Seats page in Spektrix"></iframe>';
 						break;
 					case "Basket":
 						echo '    <iframe src="' . $url . 'Basket2.aspx?' . $style . '&resize=true" onload="setTimeout(function(){ window.scrollTo(0,0);}, 100)" title="LCCC Stocker Art Center\'s Shopping Basket page in Spektrix"></iframe>';
@@ -78,7 +78,7 @@ get_header(); ?>
 						echo '    <iframe src="' . $url . 'Secure/MyAccount.aspx?' . $style . '&resize=true" onload="setTimeout(function(){ window.scrollTo(0,0);}, 100)" title="LCCC Stocker Art Center\'s MyAccount page in Spektrix"></iframe>';
 						break;
 					case "Checkout":
-						echo '    <iframe style="width:600px; min-height: 600px; border:0;" src="' . $url . 'Secure/Checkout.aspx?' . $style . '&resize=true" title="LCCC Stocker Art Center\'s Checkout page in Spektrix"></iframe>';
+						echo '    <iframe src="' . $url . 'Secure/Checkout.aspx?' . $style . '&resize=true" title="LCCC Stocker Art Center\'s Checkout page in Spektrix"></iframe>';
 						break;
 				}
 
